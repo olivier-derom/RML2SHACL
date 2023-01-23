946f4ec6183cd05b1123527cf3af81c8e5c37794
Olivier Derom <olivierd19@gmail.com>
2023-01-23 13:07:05 +0100

    updated time+memory calc and AstreaKG.ttl

--- memory_and_time.py
+++ memory_and_time.py
@@ -1,74 +1,194 @@
 import subprocess
 import psutil
-import os
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-memory = []
-cpu = []
-p = subprocess.Popen(["python", "main.py", "evaluation/astrea_test/time/mapping.ttl"], shell=True)
-# p = subprocess.Popen(["evaluation.bat"], shell=True)
-current_process = psutil.Process(p.pid)
-children = psutil.Process(p.pid).children(recursive=True)
-start = time.time()
-while p.poll() is None:
-    try:
-        currentmem = current_process.memory_info()[0]
-        currentcpu = current_process.cpu_percent()
-        for child in current_process.children(recursive=True):
-            currentmem += child.memory_info()[0]
-            currentcpu += child.cpu_percent()
-        currentmem = (currentmem / 1024 ** 2)
-        memory.append(currentmem)
-        cpu.append(currentcpu)
 
-    except:
-        pass
+# different amounts of prefixes
+self1 = ["python", "mainold.py", "evaluation/astrea_test/self2/mapping.ttl"]
+self2 = ["python", "mainold.py", "evaluation/astrea_test/self2/mapping2.ttl"]
+self3 = ["python", "mainold.py", "evaluation/astrea_test/self2/mapping3.ttl"]
+self4 = ["python", "mainold.py", "evaluation/astrea_test/self2/mapping4.ttl"]
 
-ptime = round(time.time() - start, 1)
-x = np.linspace(0, ptime, len(memory))
+self6 = ["python", "main.py", "evaluation/astrea_test/self2/mapping.ttl"]
+self7 = ["python", "main.py", "evaluation/astrea_test/self2/mapping2.ttl"]
+self8 = ["python", "main.py", "evaluation/astrea_test/self2/mapping3.ttl"]
+self9 = ["python", "main.py", "evaluation/astrea_test/self2/mapping4.ttl"]
 
-# Memory usage
-plt.figure(1)
-plt.plot(x, memory)
-plt.title('Memory usage in MiB')
-# axs[0].set_ylabel('Memory [MiB]')
-plt.xlabel('time [s]')
+old = [self1, self2, self3, self4]
+new = [self6, self7, self8, self9]
 
-# CPU usage
-plt.figure(2)
-plt.plot(x, cpu)
-plt.title('CPU usage in %')
-# axs[1].set_ylabel('CPU usage [%]')
-plt.xlabel('time [s]')
+sizes = ["17 prefixes", "13 prefixes", "10 prefixes", "6 prefixes"]
 
-# time
-print('----------------------------------')
-print(f"Total execution time in seconds: {ptime}s")
+timelist = []
+for itemindex, item in enumerate(old):
+    print(itemindex)
+    memory = []
+    p = subprocess.Popen(item, shell=True)
+    current_process = psutil.Process(p.pid)
+    children = psutil.Process(p.pid).children(recursive=True)
+    start = time.time()
+    while p.poll() is None:
+        try:
+            currentmem = current_process.memory_info()[0]
+            for child in current_process.children(recursive=True):
+                currentmem += child.memory_info()[0]
+            currentmem = (currentmem / 1024 ** 2)
+            memory.append(currentmem)
+            continue
+
+        except:
+            pass
+
+    ptime = round(time.time() - start, 1)
+    x = np.linspace(0, ptime, len(memory))
+
+    # Memory usage
+    plt.figure(1)
+    plt.plot(x, memory, label=sizes[itemindex])
+    plt.legend()
+    plt.xlabel('time [s]')
+    plt.ylabel('Memory Usage [MiB]')
+
+    # time
+    print('----------------------------------')
+    print(f"Total execution time in seconds: {ptime}s")
+    timelist.append(ptime)
+
+
+for itemindex, item in enumerate(new):
+    memory = []
+    p = subprocess.Popen(item, shell=True)
+    current_process = psutil.Process(p.pid)
+    children = psutil.Process(p.pid).children(recursive=True)
+    start = time.time()
+    while p.poll() is None:
+        try:
+            currentmem = current_process.memory_info()[0]
+            for child in current_process.children(recursive=True):
+                currentmem += child.memory_info()[0]
+            currentmem = (currentmem / 1024 ** 2)
+            memory.append(currentmem)
+            continue
+
+        except:
+            pass
+
+    ptime = round(time.time() - start, 1)
+    x = np.linspace(0, ptime, len(memory))
+
+    # Memory usage
+    plt.figure(2)
+
+    plt.plot(x, memory, label=sizes[itemindex])
+    plt.legend()
+    plt.xlabel('time [s]')
+    plt.ylabel('Memory Usage [MiB]')
+
+    print('----------------------------------')
+    print(f"Total execution time in seconds: {ptime}s")
+    timelist.append(ptime)
+
+
+# different file sizes
+self1 = ["python", "mainold.py", "evaluation/astrea_test/self/mapping.ttl"]
+self2 = ["python", "mainold.py", "evaluation/astrea_test/self/mapping3.ttl"]
+self3 = ["python", "mainold.py", "evaluation/astrea_test/self/mapping4.ttl"]
+self4 = ["python", "mainold.py", "evaluation/astrea_test/self/mapping5.ttl"]
+
+self6 = ["python", "main.py", "evaluation/astrea_test/self/mapping.ttl"]
+self7 = ["python", "main.py", "evaluation/astrea_test/self/mapping3.ttl"]
+self8 = ["python", "main.py", "evaluation/astrea_test/self/mapping4.ttl"]
+self9 = ["python", "main.py", "evaluation/astrea_test/self/mapping5.ttl"]
+
+old = [self1, self2, self3, self4]
+new = [self6, self7, self8, self9]
+
+sizes2 = ["76KB", "50KB", "34KB", "11KB"]
+
+timelist2 = []
+for itemindex, item in enumerate(old):
+    print(itemindex)
+    memory = []
+    p = subprocess.Popen(item, shell=True)
+    current_process = psutil.Process(p.pid)
+    children = psutil.Process(p.pid).children(recursive=True)
+    start = time.time()
+    while p.poll() is None:
+        try:
+            currentmem = current_process.memory_info()[0]
+            for child in current_process.children(recursive=True):
+                currentmem += child.memory_info()[0]
+            currentmem = (currentmem / 1024 ** 2)
+            memory.append(currentmem)
+            continue
+
+        except:
+            pass
+
+    ptime = round(time.time() - start, 1)
+    x = np.linspace(0, ptime, len(memory))
+
+    # Memory usage
+    plt.figure(3)
+    plt.plot(x, memory, label=sizes[itemindex])
+    plt.legend()
+    plt.xlabel('time [s]')
+    plt.ylabel('Memory Usage [MiB]')
+
+    # time
+    print('----------------------------------')
+    print(f"Total execution time in seconds: {ptime}s")
+    timelist2.append(ptime)
+
+
+for itemindex, item in enumerate(new):
+    memory = []
+    p = subprocess.Popen(item, shell=True)
+    current_process = psutil.Process(p.pid)
+    children = psutil.Process(p.pid).children(recursive=True)
+    start = time.time()
+    while p.poll() is None:
+        try:
+            currentmem = current_process.memory_info()[0]
+            for child in current_process.children(recursive=True):
+                currentmem += child.memory_info()[0]
+            currentmem = (currentmem / 1024 ** 2)
+            memory.append(currentmem)
+            continue
+
+        except:
+            pass
+
+    ptime = round(time.time() - start, 1)
+    x = np.linspace(0, ptime, len(memory))
+
+    # Memory usage
+    plt.figure(4)
+
+    plt.plot(x, memory, label=sizes[itemindex])
+    plt.legend()
+    plt.xlabel('time [s]')
+    plt.ylabel('Memory Usage [MiB]')
+
+    print('----------------------------------')
+    print(f"Total execution time in seconds: {ptime}s")
+    timelist2.append(ptime)
+
+
+for i in range(0, len(timelist)):
+    if i < 4:
+        ver = 'old'
+    else:
+        ver = 'new'
+    print(ver, sizes[i % 4], ':', timelist[i], 's')
+
+for i in range(0, len(timelist)):
+    if i < 4:
+        ver = 'old'
+    else:
+        ver = 'new'
+    print(ver, sizes2[i % 4], ':', timelist2[i], 's')
+
 plt.show()
-
-# timelist = []
-# timemax = 0
-# timemin = 9999999
-# for i in range(50):
-#
-#     p = subprocess.Popen(["evaluation1.bat"], shell=True)
-#     current_process = psutil.Process(os.getpid())
-#     children = psutil.Process(p.pid).children(recursive=True)
-#     start = time.time()
-#     while p.poll() is None:
-#         try:
-#             time.sleep(0.1)
-#         except:
-#             pass
-#     ptime = round(time.time() - start, 1)
-#     timelist.append(ptime)
-#     if ptime < timemin:
-#         timemin = (ptime*1)
-#     if ptime > timemax:
-#         timemax = (ptime*1)
-# timeavg = sum(timelist) / len(timelist)
-# print('min:',timemin)
-# print('max:',timemax)
-# print('avg:',timeavg)
