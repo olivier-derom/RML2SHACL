9d6243ffb15035330492b30d0ba0e87262e4901d
Olivier Derom <olivierd19@gmail.com>
2023-04-11 23:45:13 +0200

    fixed pytest files being able to access RML2SHACL class

--- evaluation/test-cases/RML2SHACLTC/RML2SHACL-TC1004/test_RML2SHACL-TC1004.py
+++ evaluation/test-cases/RML2SHACLTC/RML2SHACL-TC1004/test_RML2SHACL-TC1004.py
@@ -1,4 +1,6 @@
-import os
+import os 
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../.."))
 from src.RMLtoShacl import RMLtoSHACL
 from rdflib.graph import Graph
 from rdflib import compare
