ab051a6933dbd826cd5b3c6284f405385d052c0a
Olivier Derom <olivierd19@gmail.com>
2023-04-12 13:27:38 +0200

    fixed some testcases and compliance-checks in OWL enrichment

--- src/OWLtoShacl.py
+++ src/OWLtoShacl.py
@@ -157,6 +157,26 @@ class OWLtoSHACL:
             self.onto_stats[p] = 1
 
     def addOntologyConstraint(self, g, s, p, o):
+
+        if (s, self.shaclNS.datatype, None) in g:
+            datatype = g.value(subject=s, predicate=self.shaclNS.datatype)
+            print(datatype)
+            if p in {self.shaclNS.minInclusive, self.shaclNS.maxInclusive,
+                     self.shaclNS.minExclusive, self.shaclNS.maxExclusive} \
+                    and datatype in {self.XSDNS.string, self.XSDNS.NMTOKEN, self.XSDNS.Name, self.XSDNS.language,
+                                     self.XSDNS.anyURI, self.XSDNS.hexBinary, self.XSDNS.base64Binary}:
+                return
+            if p in {self.shaclNS.minLength, self.shaclNS.maxLength} \
+                    and datatype in {self.XSDNS.integer, self.XSDNS.decimal, self.XSDNS.double, self.XSDNS.long,
+                                     self.XSDNS.nonNegativeInteger, self.XSDNS.positiveInteger,
+                                     self.XSDNS.nonPositiveInteger, self.XSDNS.negativeInteger, self.XSDNS.int,
+                                     self.XSDNS.short, self.XSDNS.byte, self.XSDNS.unsignedLong, self.XSDNS.unsignedInt,
+                                     self.XSDNS.unsignedShort, self.XSDNS.unsignedByte, self.XSDNS.time,
+                                     self.XSDNS.duration, self.XSDNS.dayTimeDuration, self.XSDNS.yearMonthDuration,
+                                     self.XSDNS.gMonthDay, self.XSDNS.gYearMonth, self.XSDNS.gDay, self.XSDNS.gMonth,
+                                     self.XSDNS.gYear, self.XSDNS.dateTime, self.XSDNS.date, self.XSDNS.boolean}:
+                return
+
         if p == self.shaclNS.minInclusive:
             if (s, self.shaclNS.maxInclusive, None) in g:
                 counterRestriction = g.value(subject=s, predicate=self.shaclNS.maxInclusive)
@@ -188,7 +208,7 @@ class OWLtoSHACL:
                 counterRestriction = g.value(subject=s, predicate=self.shaclNS.maxInclusive)
                 if o >= counterRestriction:
                     return
-            if (s, self.shaclNS.minExclusive, None) in g:
+            if (s, self.shaclNS.minInclusive, None) in g:
                 return
         elif p == self.shaclNS.maxExclusive:
             if (s, self.shaclNS.minExclusive, None) in g:
@@ -199,7 +219,7 @@ class OWLtoSHACL:
                 counterRestriction = g.value(subject=s, predicate=self.shaclNS.minInclusive)
                 if o <= counterRestriction:
                     return
-            if (s, self.shaclNS.maxExclusive, None) in g:
+            if (s, self.shaclNS.maxInclusive, None) in g:
                 return
         elif p == self.shaclNS.minLength:
             if (s, self.shaclNS.maxLength, None) in g:
